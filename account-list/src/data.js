2ea017935d024ba24d9f88e60337b4927dcd64c9
eakyel <e.akyel@outlook.com>
2022-01-24 19:49:59 +0300

    UPDADE (account-list): Create fake data

---
+++ account-list/src/data.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    accountName: "Dropbox",
+    accountWebsite: "www.dropbox.com",
+    accountPassword: "423512",
+  },
+  {
+    id: 2,
+    accountName: "Paypal",
+    accountWebsite: "www.paypal.com",
+    accountPassword: "681242",
+  },
+  {
+    id: 3,
+    accountName: "Ziraat Bankası",
+    accountWebsite: "www.ziraat.com",
+    accountPassword: "9382741",
+  },
+];
