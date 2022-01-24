498380ef402851ea533fb4298cfb0b9669faa804
eakyel <e.akyel@outlook.com>
2022-01-24 19:54:03 +0300

    UPDATE (account-list): Update account component to render dynamically

--- account-list/src/components/Account/index.js
+++ account-list/src/components/Account/index.js
@@ -2,18 +2,35 @@ import React from "react";
 import "./Account.css";
 import dots from "../../images/dots.png";
 
-export default function Account() {
+export default function Account(props) {
+  function computeImageName(name, dataFormat = "png") {
+    // Convert to lowercase first letter
+    let imageName;
+
+    // If includes space in prop.accountName; replace the space char with '-' char
+    name.includes(" ")
+      ? (imageName = name.toLowerCase().split(" ").join("-"))
+      : (imageName = name.toLowerCase());
+
+    return imageName + "." + dataFormat;
+  }
+
   return (
     <article className="account">
       <img
         className="account__logo"
-        src={process.env.PUBLIC_URL + "/images/dropbox.png"}
+        src={
+          process.env.PUBLIC_URL +
+          `/images/${computeImageName(props.accountName, "png")}`
+        }
       />
       <div className="account__platform">
-        <h3 className="platform__title">Dropbox</h3>
-        <span className="platform__link">www.dropbox.com</span>
+        <h3 className="platform__title">{props.accountName}</h3>
+        <a className="platform__link" href={"https://" + props.accountWebsite}>
+          {props.accountWebsite}
+        </a>
       </div>
-      <h4 className="account__password">123456</h4>
+      <h4 className="account__password">{props.accountPassword}</h4>
       <button className="account__settings">
         <img src={dots} alt="" />
       </button>
